13869de55cbaafbde863e130789df138b7dc5130
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-03 15:48:12 +0200

    create about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import Navigation from "./navigation/Navigation";
+import About from "./hero-section/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
+      <About />
     </BrowserRouter>
   );
 }
